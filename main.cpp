e1c3978aa9b18c4570d51ef461e142229771b583
zeizize <152731331+zeizize@users.noreply.github.com>
2023-12-02 15:58:56 -0500

    hw4..

--- main.cpp
+++ main.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 
-using namespace std;  
+using namespace std;
 
 int main() {
-float x,y,z,A,B,C,D,E;
+int x,y,z;
 cout<<"enter the value of x:  " ;
 cin>>x;
 cout<<"enter the value of y:  " ;
 cin>>y;
 cout<<"enter the value of z:  " ;
 cin>>z;
-A=(x-y)*(x+z);
-B=(x+y+z);
-C=(B*B);
-D=(C+1);
-E=(x-y)*(x+z)/(D);
-cout<<E;
+int result1=(x-y)*(x+z);
+int result2=(x+y+z)*(x+y+z)+(1);
+int finalresult=(x-y)*(x+z)/(x+y+z)*(x+y+z)+(1);
+cout<<finalresult<<endl;
 }
