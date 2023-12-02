5fe9ac0880a1fa201eb424d231bdae7d8fa68703
zeizize <152731331+zeizize@users.noreply.github.com>
2023-12-02 15:06:20 -0500

    hw4

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+float m,n,A,B,C,D;
+cout<<"please enter two decimal numbers  : ";
+cin>>m>>n;
+A=m+n;
+
+B=m-n;
+
+C=m*n;
+
+D=m/n;
+
+cout<<"plus"<< A <<"/n"<<"minus"<< B <<"/n"<<"multiply"<< C <<"/n"<<"divided by"<< D;
+}
