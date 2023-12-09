2358bb70498d7c3e71ddd3abe3e71e8c5ef3a9e4
zeizize <152731331+zeizize@users.noreply.github.com>
2023-12-09 16:45:52 -0500

    Add files via upload

---
+++ hw5.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int salary,tax;
+	cout<<"please enter the salary : " ;
+	cin>>salary;
+	if(salary < 6000000) {
+		tax=salary*0;
+		 salary=salary-tax;
+		 cout<<"your salary with tax is :"<<salary;
+	}
+    else if(salary >= 6000000 && salary < 8000000 ) {
+    	 tax=salary*0.05;
+    	 salary=salary-tax;
+    	 cout<<"your salary with tax is :"<<salary;
+	}
+    else if(salary >= 8000000 && salary < 10000000) {
+    	tax=salary*0.10;
+    	 salary=salary-tax;
+		cout<<"your salary with tax is :"<<salary;
+	}
+	else if(salary >= 10000000 && salary < 14000000) {
+		tax=salary*0.15;
+		 salary=salary-tax;
+		  cout<<"your salary with tax is :"<<salary;
+	}
+	else if(salary >= 14000000 && salary < 18000000) {
+		tax=salary*0.20;
+		 salary=salary-tax;
+		  cout<<"your salary with tax is :"<<salary;
+	}
+	else if(salary >= 18000000 && salary < 25000000) {
+		tax=salary*0.25;
+		 salary=salary-tax;
+		cout<<"your salary with tax is :"<<salary;
+	}
+	else { 
+	salary > 25000000;
+		tax=salary*0.35;
+		 salary=salary-tax;
+		  cout<<"your salary with tax is :"<<salary;
+}
+
+}
